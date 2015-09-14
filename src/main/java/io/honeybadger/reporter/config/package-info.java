71144a528c97a7c64e7ad9eb2bcd63e64e94248e
Elijah Zupancic <elijah@zupancic.name>
2015-09-14 09:48:19 -0700

    Fixed javadoc.

--- src/main/java/io/honeybadger/reporter/config/package-info.java
+++ src/main/java/io/honeybadger/reporter/config/package-info.java
@@ -2,7 +2,7 @@
  * Configuration class are contained in this package.
  *
  * The usage pattern is that you will define an implementation of
- * @{link ConfigContext} that provides a default values implementation
+ * {@link io.honeybadger.reporter.config.ConfigContext} that provides a default values implementation
  * and then you will subclass {@link io.honeybadger.reporter.config.BaseChainedConfigContext}
  * and use the method overwriteWithContext() to load the default values into
  * the class and then load that specific configuration's values on top of the
