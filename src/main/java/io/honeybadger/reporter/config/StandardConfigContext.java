d1ed5af7c37702b9c3756fd0df399741ce14ab4d
Elijah Zupancic <elijah@zupancic.name>
2015-09-14 09:24:08 -0700

    Added standard constructors to standard config context.

--- src/main/java/io/honeybadger/reporter/config/StandardConfigContext.java
+++ src/main/java/io/honeybadger/reporter/config/StandardConfigContext.java
@@ -11,12 +11,36 @@ import java.util.Map;
  * @since 1.0.10
  */
 public class StandardConfigContext extends BaseChainedConfigContext {
+    /**
+     * A new configuration context with default values prepopulated.
+     */
     public StandardConfigContext() {
         super();
     }
 
+    /**
+     * A new configuration context with default values present, but overwritten
+     * by the passed map of configuration values.
+     *
+     * @param configuration map with keys that correspond to the sys prop keys documented
+     */
     public StandardConfigContext(Map<String, ?> configuration) {
         super();
         overwriteWithContext(new MapConfigContext(configuration));
+    }
+
+    /**
+     * A new configuration context with the default values present. Only the
+     * API key is set.
+     *
+     * @param apiKey Honeybadger API key
+     */
+    public StandardConfigContext(String apiKey) {
+        if (apiKey == null) throw new IllegalArgumentException(
+                "API key must not be null");
+        if (apiKey.isEmpty()) throw new IllegalArgumentException(
+                "API key must not be empty");
+
+        setApiKey(apiKey);
     }
 }
