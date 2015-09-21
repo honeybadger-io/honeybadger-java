663cd72730bfa118842e168db5de82be57365e2a
Elijah Zupancic <elijah@zupancic.name>
2015-09-20 22:42:12 -0700

    Added API three time timeout check.

--- src/main/java/io/honeybadger/loader/HoneybadgerNoticeLoader.java
+++ src/main/java/io/honeybadger/loader/HoneybadgerNoticeLoader.java
@@ -6,12 +6,13 @@ import com.google.gson.JsonObject;
 import io.honeybadger.reporter.HoneybadgerExclusionStrategy;
 import io.honeybadger.reporter.config.ConfigContext;
 import io.honeybadger.reporter.dto.Notice;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URI;
 import java.util.UUID;
 
@@ -49,12 +50,36 @@ public class HoneybadgerNoticeLoader {
 
         logger.debug("Querying for error details: {}", baseURI);
 
-        Response response = Request
-                .Get(withAuth)
-                .addHeader("Accept", "application/json")
-                .execute();
+        Response response = null;
+        HttpResponse httpResponse = null;
 
-        return response.returnContent().asString();
+        // We loop here because the API returns 404 when the notice still
+        // hasn't finished processing
+        for (int i = 0; i < 3; i++) {
+            response = Request.Get(withAuth)
+                              .addHeader("Accept", "application/json")
+                              .execute();
+
+            httpResponse = response.returnResponse();
+
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                break;
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+
+            if  (i == 2) {
+                throw new IllegalArgumentException("Unable to get notice from API");
+            }
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        httpResponse.getEntity().writeTo(out);
+
+        return out.toString();
     }
 
     public Notice findErrorDetails(UUID faultId) throws IOException {
